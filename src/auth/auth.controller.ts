b5b6ab864235f877bf88b09683621a8b57be1226
MaxRyazan <no4ta87@mail.ru>
2023-11-11 21:05:47 +0300

    1.1.3  registration added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,13 +2,21 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from './auth.service';
 import { AuthDto } from "./dto/authDto";
 import { Tokens } from "../globalInterfaces/interfaces";
+import * as process from "process";
+import { RegisterDto } from "./dto/registerDto";
+import { IUser } from "../globalClasses/IUser";
 
 @Controller('/')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post(`/v1/auth`)
+  @Post(`/${process.env.VERSION}/auth`)
   login(@Body() authDto: AuthDto):Promise<Tokens> {
     return this.authService.login(authDto)
+  }
+
+  @Post(`/${process.env.VERSION}/register`)
+  register(@Body() registerDto: RegisterDto):Promise<IUser> {
+    return this.authService.register(registerDto)
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { AuthDto } from "./dto/authDto";
 import { USER_DB } from "../../USERS";
 import { IUser } from "../globalClasses/IUser";
 import { Tokens } from "../globalInterfaces/interfaces";
 import * as bcrypt from "bcrypt"
 import { JwtService } from "@nestjs/jwt";
+import { RegisterDto } from "./dto/registerDto";
 
 @Injectable()
 export class AuthService {
@@ -27,6 +28,19 @@ export class AuthService {
     } else {
       throw new NotFoundException('Неверные данные пользователя!')
     }
+  }
+
+  async register(registerDto: RegisterDto): Promise<IUser> {
+    if (registerDto.password !== registerDto.confirmPassword) {
+      throw new BadRequestException('Пароли не совпадают!')
+    }
+    const existingUser: IUser | null = USER_DB.find((user: IUser): boolean => user.email === registerDto.email)
+    if (existingUser) {
+      throw new BadRequestException('Пользователь с такими данными уже зарегистрирован!')
+    }
+    const newUser:IUser = new IUser(registerDto.username, existingUser.email, await bcrypt.hash(registerDto.password, 5))
+    USER_DB.push(newUser)
+    return newUser
   }
 
 
